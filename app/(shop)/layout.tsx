98a8911c4637a3fa1514ace8f645116156cdc617
nima.yaghouti <nima.yaghouti99@gmail.com>
2026-06-05 21:26:20 +0330

    feat: add Header and Footer to ShopLayout wrapper for main application routes

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -1,5 +1,16 @@
-export default function Layout({
+import { Footer } from '@/components/layout/Footer';
+import { Header } from '@/components/layout/Header';
+
+export default function ShopLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <main>{children}</main>;
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1 container mx-auto px-4 md:px-8 py-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
 }
