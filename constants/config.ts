d26919335c5ca60f31f2cb57f6d44e184179b0e6
nima.yaghouti <nima.yaghouti99@gmail.com>
2026-06-16 18:15:00 +0330

    refactor: remove unused constants directory adhering to YAGNI principle

--- constants/config.ts
+++
@@ -1,8 +0,0 @@
-export const DATA_URL =
-  'https://raw.githubusercontent.com/nimayaghouti/booktabdata/main/db.json';
-
-export const SITE_CONFIG = {
-  name: 'Varaq',
-  description: 'کتابفروشی آنلاین ورق',
-  url: 'http://localhost:3000',
-};
